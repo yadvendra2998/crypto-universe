f4880b6a4523ab449a2f76101a6f5f3595a5529c
Yadvendra Yadav <yadvendra2998@gmail.com>
2022-01-02 21:16:49 +0530

    Update with chart

---
+++ src/components/LineChart.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Tooltip,
+    Legend,
+  } from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import { Col, Row, Typography } from 'antd';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Tooltip,
+    Legend
+);
+
+const { Title } = Typography;
+
+const LineChart = ({ coinHistory, currentPrice, coinName }) => {
+  const coinPrice = [];
+  const coinTimestamp = [];
+
+  //console.log(coinHistory.data.history);
+  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+    coinPrice.push(coinHistory?.data?.history[i].price);
+  }
+  //console.log(coinPrice);
+
+  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+    coinTimestamp.push(new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString());
+  }
+  //console.log(coinTimestamp);
+
+  const data = {
+    labels: coinTimestamp,
+    datasets: [
+      {
+        label: 'Price In USD',
+        data: coinPrice,
+        fill: false,
+        backgroundColor: '#0071bd',
+        borderColor: '#0071bd',
+        yAxisID: 'y',
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+        y: {
+          type: 'linear',
+          display: true,
+          position: 'left',
+        }
+    }
+};
+
+  return (
+    <>
+      <Row className="chart-header">
+        <Title level={2} className="chart-title">{coinName} Price Chart </Title>
+        <Col className="price-container">
+          <Title level={5} className="price-change">Change: {coinHistory?.data?.change}%</Title>
+          <Title level={5} className="current-price">Current {coinName} Price: $ {currentPrice}</Title>
+        </Col>
+      </Row>
+      <Line data={data} options={options} />
+    </>
+  );
+};
+
+export default LineChart;
